4e5b2c25221da94292b5f4bf0789839e18bd6ac5
Kristoffer Hansson <97549161+Zarser@users.noreply.github.com>
2022-04-21 23:24:07 +0200

    Version 2, ny layout, mer content
    Gjort egna "vägskyltar", fixat "egna" ljudfiler till vägskyltarna.. mm

---
+++ app.js
@@ -0,0 +1,37 @@
+function play1() {
+    var audio = document.getElementById("narlunda");
+    audio.play();
+  }
+
+  function play2() {
+    var audio = document.getElementById("rosengarden");
+    audio.play();
+  }
+
+  function play3() {
+    var audio = document.getElementById("ringstorp");
+    audio.play();
+  }
+  function play4() {
+    var audio = document.getElementById("rosenknoppen");
+    audio.play();
+  }
+
+  function play5() {
+    var audio = document.getElementById("rydeback");
+    audio.play();
+  }
+
+  function play6() {
+    var audio = document.getElementById("Skoldenborg");
+    audio.play();
+  }
+  function play7() {
+    var audio = document.getElementById("soder");
+    audio.play();
+  }
+
+  function play8() {
+    var audio = document.getElementById("tagaborg");
+    audio.play();
+  }
